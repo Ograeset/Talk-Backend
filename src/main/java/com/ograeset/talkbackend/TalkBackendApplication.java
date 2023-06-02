9fc74f9fe5f85e799fb8d474151883ad70766026
Ograeset <tedros963@gmail.com>
2023-06-02 10:32:21 +0200

    first commit

---
+++ src/main/java/com/ograeset/talkbackend/TalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ograeset.talkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ograeset/talkbackend/TalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ograeset.talkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
